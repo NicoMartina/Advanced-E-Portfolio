c0fbb6a9f78111fa0086d80bf7b6854ce964fb4a
NicoMartina <nicolas.martina69@gmail.com>
2022-04-12 11:33:56 +0200

    editing JS

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 function contact(event) {
-	// event.preventDefault();
-	// emailjs
-	// 	.sendForm(
-	// 		"service_ffljnj8",
-	// 		"template_q626vz7",
-	// 		event.target,
-	// 		"HpsE4Y3FFSl6SjygY"
-	// 	)
-	// 	.then(() => {
-	// 		console.log("it works!");
-	// 	});
+	event.preventDefault();
 	const loading = document.querySelector(".modal__overlay--loading");
 	const success = document.querySelectorAll(".modal__overlay--success");
 	loading.classList += " modal__overlay--visible";
-	setTimeout(() => {
-		loading.classList.remove("modal__overlay--visible");
-		success.classList += " modal__overlay--visible";
-		console.log("it works!");
-	}, 1000);
+	emailjs
+		.sendForm(
+			"service_ffljnj8",
+			"template_q626vz7",
+			event.target,
+			"HpsE4Y3FFSl6SjygY"
+		)
+		.then(() => {
+			loading.classList.remove("modal__overlay--visible");
+			success.classList += " modal__overlay--visible";
+		})
+		.catch(() => {
+			loading.classList.remove("modal__overlay--visible");
+			alert(
+				"The email service is temporarily unavailable. Please contact me directly at nicolas.martina69@gmail.com"
+			);
+		});
 }
